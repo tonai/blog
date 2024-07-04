07d33f6dc1eaa00f58dbdafebf252bc8cf5b9de0
Tony CABAYE <tony.cabaye@smile.fr>
2024-07-04 12:16:29 +0200

    Deploy website - based on a4a57d74160398d975bfd5f10f9ea372fbe2c2db

---
+++ assets/js/5b150f25.8c1dc76b.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkblog=self.webpackChunkblog||[]).push([[685],{3313:e=>{e.exports=JSON.parse('{"metadata":{"permalink":"/blog/posts","page":1,"postsPerPage":10,"totalPages":1,"totalCount":5,"blogDescription":"Blog","blogTitle":"Blog"}}')}}]);
